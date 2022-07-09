59e1cf16dfd32c6e572d9f7ae561f761d029723d
Conner Foster <connerf99@gmail.com>
2022-07-09 18:54:46 -0400

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,37 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date().toDateString()
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date().toDateString(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date().toDateString()
-  }
-];
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date().toDateString(),
+  },
+]
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Mini Messageboard', messages: messages})
+})
 
 /* GET home page. */
-router.get('/new', function(req, res, next) {
-  res.render('form');
-});
+router.get('/new', function (req, res, next) {
+  res.render('form')
+})
 
+router.post('/new', function (req, res, next) {
+  const user = req.body.name
+  const msg = req.body.usermsg
+  const dateAdded = new Date().toDateString()
 
-module.exports = router;
+  messages.push({text: msg, user: user, added: dateAdded})
+
+  res.redirect('/')
+})
+
+module.exports = router
